1e53df54c6988ddc8ee5e2016fdfb3f0eed03ad8
ma6174 <ma6174@163.com>
2013-08-24 14:20:11 +0800

    修复安装权限问题

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo git clone https://github.com/ma6174/speak_raspi_ip.git
 sudo mv speak_raspi_ip /var/
-sudo echo "/usr/bin/python /var/speak_raspi_ip/speak_ip.py &" >> /etc/rc.local
+echo "/usr/bin/python /var/speak_raspi_ip/speak_ip.py &" | sudo tee -a /etc/rc.local
 echo "install finished!"
