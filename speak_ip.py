059066b5a8f766858c2f8b83de93b048cfd8c093
ma6174 <ma6174@163.com>
2013-08-24 11:41:36 +0800

    升级获取IP方法，更新README

--- speak_ip.py
+++ speak_ip.py
@@ -4,40 +4,57 @@
 import os
 import time
 import socket
+import subprocess
 
+voice_path = "/var/speak_ip/voice"
+player = "omxplayer"
 
-voice_path = "/home/pi/speak_ip/voice"
 
 def getLocalIP():
+    ip = None
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('114.114.114.114', 0))
-        return s.getsockname()[0]
+        ip =  s.getsockname()[0]
     except:
         name = socket.gethostname()
-        ip_addr = socket.gethostbyname(name)
-        return ip_addr
+        ip = socket.gethostbyname(name)
+    if ip.startswith("127."):
+        cmd = '''/sbin/ifconfig | grep "inet " | cut -d: -f2 | awk '{print $1}' | grep -v "^127."'''
+        a = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+        a.wait()
+        out = a.communicate()
+        ip = out[0].strip().split("\n") # 所有的列表
+        ip = "完".join(ip)
+    return ip
+
 
 def getFilePath(filename):
-    return  os.path.join(voice_path,"%s.mp3"%filename)
+    return os.path.join(voice_path, "%s.mp3" % filename)
+
+
+def play(voice):
+    os.system("%s %s > /dev/null 2>&1" % (player, getFilePath(voice)))
+
 
 def speak(ip):
-   for i in ip:
-       if i == ".":
-           os.system("mpg123 %s > /dev/null 2>&1"%getFilePath("点"))
-       else:
-           os.system("mpg123 %s > /dev/null 2>&1"%getFilePath(i))
-   os.system("mpg123 %s > /dev/null 2>&1"%getFilePath("完"))
+    for i in ip:
+        if i == ".":
+            play("点")
+        else:
+            play(i)
+    play("完")
 
-count = 0
-while True:
-    ip = getLocalIP()
-    print ip
-    if ip.startswith("127"):
-        os.system("mpg123 %s > /dev/null 2>&1"%getFilePath("正在获取网络地址"))
-    else:
-        count += 1
-        speak(ip)
-    if count == 10:
-        break
-    time.sleep(1)
+if __name__=='__main__':
+    count = 0
+    while True:
+        ip = getLocalIP()
+        print ip
+        if ip.startswith("127"):
+            play("正在获取网络地址")
+        else:
+            count += 1
+            speak(ip)
+        if count == 10:
+            break
+        time.sleep(1)
